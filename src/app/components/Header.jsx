7c906e7f95888f547b7ad569dc468712eaf7e0d7
Yasirhassan1 <yasuhassan7896@gmail.com>
2025-08-28 23:12:11 +0500

    add header

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+export default function Header(){
+    return(
+        <header className="flex justify-between gap-3 h-[50px] bg-black text-white items-center px-4">
+          <h1>LOGO</h1>
+          <div className="center flex gap-3">
+            <Link href={"/"}>Home</Link>
+            <Link href={"/"}>About</Link>
+            <Link href={"/"}>Contact</Link>
+          </div>
+          <button className="bg-amber-300 px-4 py-2 rounded-full">Sign Up</button>
+        </header>
+    )
+}
